44581a964832faf11d252457c6065437de6df876
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 15:58:49 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def cobble
+    puts "The shoes have been repaired."
+    Shoe.condition = "new"
+  end
+
+end
