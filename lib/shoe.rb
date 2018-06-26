fc2c34bcb9ed4e506069e8f07beb91b1b1d92389
Marissa Zhong <mwz2101@columbia.edu>
2018-06-26 16:10:52 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def cobble(shoe)
+  def cobble
     puts "Your shoe is as good as new!"
     @condition = "new"
   end
